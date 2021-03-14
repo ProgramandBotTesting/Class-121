bfc133e8a03aff643ed4aae392935771b488ec84
Bloomss <>
2021-03-14 09:56:45 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,39 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+
+  video = createCapture(VIDEO);
+  video.hide();
+
+  classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+}
+
+function preload() {
+
+}
+
+function draw() {
+  image(video, 0, 0, 300, 300);
+
+  classifier.classify(video, gotResult);
+}
+
+function modelLoaded() {
+  try {
+    console.log("Model loaded successfully.");
+  } catch (err) {
+    console.log(err);
+    console.log("Model did not load.");
+  }
+}
+
+function gotResult(err, results) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
+}
+
